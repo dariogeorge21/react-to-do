9866ae62928c57edd72a3272d031304f3abbde57
Dario George <edu.dariogeorge21@gmail.com>
2025-06-02 14:47:18 +0530

    Configure for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-to-do/', // Replace 'react-to-do' with your GitHub repository name
+  base: '/react-to-do/',
   build: {
     outDir: 'dist',
   },
